f1e39678a5e9973a90bc36b488966a1d42c2aa7d
tracer-ps4-2021 <tracer-ps4-2021@ics.nju.edu.cn>
2021-03-13 12:32:01 +0800

    compile

--- source/DataStructures/Graph.cpp
+++ source/DataStructures/Graph.cpp
@@ -110,10 +110,12 @@ bool Graph::RemoveEdge(int vertex1, int vertex2) {
             return false;
         } else {
             auto in = find(edges_in[vertex_map[vertex2]].begin(),edges_in[vertex_map[vertex2]].end(),e);
-            edges_in[vertex_map[vertex2]].erase(in);
+            if(in != edges_in[vertex_map[vertex2]].end())
+                edges_in[vertex_map[vertex2]].erase(in);
 
             auto out = find(edges_out[vertex_map[vertex1]].begin(),edges_out[vertex_map[vertex1]].end(),e);
-            edges_out[vertex_map[vertex1]].erase(in);
+            if(out != edges_out[vertex_map[vertex1]].end())
+                edges_out[vertex_map[vertex1]].erase(in);
 
             edges.erase(it);
             return true;
