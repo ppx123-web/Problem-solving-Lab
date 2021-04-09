624e7dab393826941260894abd6a89ab8107ce1d
tracer-ps4-2021 <tracer-ps4-2021@ics.nju.edu.cn>
2021-04-09 18:18:39 +0800

    compile

--- tests/bfs_dfs.cpp
+++ tests/bfs_dfs.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main() {
     UndirectedGraph g;
     assert(g.AddVertex(1));
-    for(int i = 2; i <= 3; ++i) {
+    for(int i = 2; i <= 5; ++i) {
         assert(g.AddVertex(i));
         assert(g.AddEdge(i - 1, i));
     }
