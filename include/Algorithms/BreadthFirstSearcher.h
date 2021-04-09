6e19cdf634b351bb2f6226b81804cd7546fd17fe
tracer-ps4-2021 <tracer-ps4-2021@ics.nju.edu.cn>
2021-04-09 17:03:26 +0800

    compile

--- include/Algorithms/BreadthFirstSearcher.h
+++ include/Algorithms/BreadthFirstSearcher.h
@@ -18,4 +18,46 @@ class BreadthFirstSearcher {
     static std::optional<int> FindFirstVertex(const TGraph *graph, int start, const std::function<bool(int)> &predicate);
 };
 
+template<typename TGraph>
+void BreadthFirstSearcher<TGraph>::VisitAllVertices(const TGraph *graph, int start, const function<void(int)> &action) {
+    unordered_map<int,int> vis;
+    queue<int> q;
+    q.push(start);
+    action(start);
+    while (!q.empty()) {
+        int cur = q.front();
+        q.pop();
+        vis[cur] = 1;
+        for(int u : graph->GetNeighbors(cur)) {
+            if(vis.find(u) == vis.end()) {
+                q.push(u);
+                action(u);
+            }
+        }
+    }
+}
+
+template<typename TGraph>
+std::optional<int>
+BreadthFirstSearcher<TGraph>::FindFirstVertex(const TGraph *graph, int start, const function<bool(int)> &predicate) {
+    unordered_map<int,int> vis;
+    queue<int> q;
+    q.push(start);
+    if(predicate(start)) return start;
+    while (!q.empty()) {
+        int cur = q.front();
+        q.pop();
+        vis[cur] = 1;
+        for(int u : graph->GetNeighbors(cur)) {
+            if(vis.find(u) == vis.end()) {
+                q.push(u);
+                if(predicate(u)) {
+                    return u;
+                }
+            }
+        }
+    }
+    return std::optional<int>();
+}
+
 #endif
