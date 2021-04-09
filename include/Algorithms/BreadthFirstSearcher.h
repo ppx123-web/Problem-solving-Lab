a4cfd9f6113e21099c1a23fb83d5e4ca0eb9fd49
tracer-ps4-2021 <tracer-ps4-2021@ics.nju.edu.cn>
2021-04-09 17:23:20 +0800

    compile

--- include/Algorithms/BreadthFirstSearcher.h
+++ include/Algorithms/BreadthFirstSearcher.h
@@ -23,15 +23,14 @@ void BreadthFirstSearcher<TGraph>::VisitAllVertices(const TGraph *graph, int sta
     unordered_map<int,int> vis;
     queue<int> q;
     q.push(start);
-    action(start);
     while (!q.empty()) {
         int cur = q.front();
         q.pop();
         vis[cur] = 1;
+        action(cur);
         for(int u : graph->GetNeighbors(cur)) {
             if(vis.find(u) == vis.end()) {
                 q.push(u);
-                action(u);
             }
         }
     }
@@ -43,17 +42,16 @@ BreadthFirstSearcher<TGraph>::FindFirstVertex(const TGraph *graph, int start, co
     unordered_map<int,int> vis;
     queue<int> q;
     q.push(start);
-    if(predicate(start)) return start;
     while (!q.empty()) {
         int cur = q.front();
         q.pop();
         vis[cur] = 1;
+        if(predicate(cur)) {
+            return cur;
+        }
         for(int u : graph->GetNeighbors(cur)) {
             if(vis.find(u) == vis.end()) {
                 q.push(u);
-                if(predicate(u)) {
-                    return u;
-                }
             }
         }
     }
