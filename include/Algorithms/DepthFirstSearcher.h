7e14e874b195c4ecf435368b8e3ba9f6a5e79c95
tracer-ps4-2021 <tracer-ps4-2021@ics.nju.edu.cn>
2021-04-09 16:42:56 +0800

    compile

--- include/Algorithms/DepthFirstSearcher.h
+++ include/Algorithms/DepthFirstSearcher.h
@@ -3,12 +3,28 @@
 
 #include <functional>
 #include <optional>
+#include <DataStructures/Graph.h>
+#include <DataStructures/WeightedGraph.h>
+#include <DataStructures/UndirectedGraph.h>
+#include <DataStructures/UndirectedWeightedGraph.h>
+#include <queue>
+#include <unordered_map>
+using namespace std;
+
 
 template <typename TGraph>
 class DepthFirstSearcher {
  public:
-  static void VisitAllVertices(const TGraph *graph, int start, const std::function<void(int)> &action);
-  static std::optional<int> FindFirstVertex(const TGraph *graph, int start, const std::function<bool(int)> &predicate);
+    static void VisitAllVertices(const TGraph *graph, int start, const std::function<void(int)> &action) {
+        unordered_map<int,int> vis;
+        vis[start] = 1;
+        for(const int u:graph->GetNeighbors(cur)) {
+            if()
+        }
+    }
+
+
+    static std::optional<int> FindFirstVertex(const TGraph *graph, int start, const std::function<bool(int)> &predicate);
 };
 
 #endif
