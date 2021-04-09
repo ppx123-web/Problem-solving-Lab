ba32a33b193fc8b8d3c30d55e37b307eab40f5a3
tracer-ps4-2021 <tracer-ps4-2021@ics.nju.edu.cn>
2021-04-09 17:54:02 +0800

    compile

--- tests/bfs_dfs.cpp
+++ tests/bfs_dfs.cpp
@@ -8,11 +8,11 @@ using namespace std;
 int main() {
     UndirectedGraph g;
     assert(g.AddVertex(1));
-    for(int i = 2; i <= 5; ++i) {
+    for(int i = 2; i <= 10; ++i) {
         assert(g.AddVertex(i));
         assert(g.AddEdge(i - 1, i));
     }
-    BreadthFirstSearcher<Graph>::VisitAllVertices(&g, 3, [] (int a) {
+    BreadthFirstSearcher<Graph>::VisitAllVertices(&g, 1, [] (int a) {
         cout << a << endl;
         //return a == 3;
     });
