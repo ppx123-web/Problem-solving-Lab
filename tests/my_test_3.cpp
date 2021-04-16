db7d800b799d4f8b57872a79452ed58ae7ee95b5
tracer-ps4-2021 <tracer-ps4-2021@ics.nju.edu.cn>
2021-04-16 20:33:46 +0800

    compile

--- tests/my_test_3.cpp
+++ tests/my_test_3.cpp
@@ -3,8 +3,12 @@
 
 int main() {
     WeightedGraph<int> *g = new WeightedGraph<int>();
-    g->AddVertex(1), g->AddVertex(2);
+    g->AddVertex(1);
+    g->AddVertex(2);
+    g->AddVertex(3);
     g->AddEdge(1, 2, 100);
+    g->AddEdge(1, 3, 100);
+    g->AddEdge(3, 4, 100);
 
     ShortestPaths<WeightedGraph, int>
             *p = new DijkstraShortestPaths<WeightedGraph, int>(g, 1);
