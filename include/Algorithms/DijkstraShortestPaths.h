861488d6b9611b99e0f3ea637416ec25f05aba52
tracer-ps4-2021 <tracer-ps4-2021@ics.nju.edu.cn>
2021-04-16 20:29:01 +0800

    compile

--- include/Algorithms/DijkstraShortestPaths.h
+++ include/Algorithms/DijkstraShortestPaths.h
@@ -6,15 +6,11 @@
 
 template<template<typename> class TGraph, typename TValue>
 class DijkstraShortestPaths : public ShortestPaths<TGraph, TValue> {
-private:
-    unordered_map<int, TValue> vis;
-    unordered_map<int, TValue> cost;
-    unordered_map<int, TValue> parent;
-    int source;
+
 public:
     DijkstraShortestPaths(TGraph<TValue> *graph, int src);
 
-    ~DijkstraShortestPaths();
+    ~DijkstraShortestPaths() = default;
 
 public:
     bool HasPathTo(int destination) const;
@@ -33,62 +29,47 @@ public:
 
 template<template<typename> class TGraph, typename TValue>
 DijkstraShortestPaths<TGraph, TValue>::DijkstraShortestPaths(TGraph<TValue> *graph, int src) {
-    vis.clear();
-    cost.clear();
-    parent.clear();
-    source = src;
+    ShortestPaths<TGraph,TValue>::vis.clear();
+    ShortestPaths<TGraph,TValue>::cost.clear();
+    ShortestPaths<TGraph,TValue>::parent.clear();
+    ShortestPaths<TGraph,TValue>::source = src;
     priority_queue<pair<int, TValue>, vector<pair<int, TValue>>, cmp> pq;
-    cost[src] = TValue();
-    pq.emplace(src, cost[src]);
+    ShortestPaths<TGraph,TValue>::cost[src] = TValue();
+    pq.emplace(src, ShortestPaths<TGraph,TValue>::cost[src]);
     while (!pq.empty()) {
         const pair<int, TValue> temp = pq.top();
         const int cur_idx = temp.first;
         const TValue cur_cost = temp.second;
         pq.pop();
-        if (vis[cur_idx]) continue;
-        vis[cur_idx] = 1;
+        if (ShortestPaths<TGraph,TValue>::vis[cur_idx]) continue;
+        ShortestPaths<TGraph,TValue>::vis[cur_idx] = 1;
         for (auto next:graph->GetOutgoingEdges(cur_idx)) {
             if (next.GetDestination() == cur_idx) continue;
             const int new_idx = next.GetDestination();
             const TValue new_cost = cur_cost + next.GetWeight();
-            if (new_cost < cost[new_idx]) {
+            if (new_cost < ShortestPaths<TGraph,TValue>::cost[new_idx]) {
                 pq.emplace(new_idx, new_cost);
-                cost[new_idx] = new_cost;
-                parent[new_idx] = cur_idx;
+                ShortestPaths<TGraph,TValue>::cost[new_idx] = new_cost;
+                ShortestPaths<TGraph,TValue>::parent[new_idx] = cur_idx;
             }
         }
     }
 }
 
-template<template<typename> class TGraph, typename TValue>
-DijkstraShortestPaths<TGraph, TValue>::~DijkstraShortestPaths() {
-
-}
 
 template<template<typename> class TGraph, typename TValue>
 bool DijkstraShortestPaths<TGraph, TValue>::HasPathTo(int destination) const {
-    return vis.find(destination) != vis.end();
+    return ShortestPaths<TGraph,TValue>::HasPathTo(destination)
 }
 
 template<template<typename> class TGraph, typename TValue>
 optional<TValue> DijkstraShortestPaths<TGraph, TValue>::TryGetDistanceTo(int destination) const {
-    if(vis.find(destination) != vis.end()) {
-        return optional<TValue>(cost.find(destination)->second);
-    } else return nullopt;
+    return ShortestPaths<TGraph,TValue>::TryGetDistanceTo(destination);
 }
 
 template<template<typename> class TGraph, typename TValue>
 std::optional<std::vector<int>> DijkstraShortestPaths<TGraph, TValue>::TryGetShortestPathTo(int destination) const {
-    vector<int> ans;
-    int cur = destination;
-    if(vis.find(destination) != vis.end()) {
-        while (cur != source) {
-            ans.insert(ans.begin(),cur);
-            cur = parent.find(cur)->first;
-        }
-        ans.insert(ans.begin(),cur);
-        return ans;
-    } else return nullopt;
+    return ShortestPaths<TGraph,TValue>::TryGetShortestPathTo(destination);
 }
 
 /*template<template<typename> class TGraph, typename TValue>
--- include/Algorithms/ShortestPaths.h
+++ include/Algorithms/ShortestPaths.h
@@ -10,7 +10,11 @@ using namespace std;
 
 template<template<typename> class TGraph, typename TValue>
 class ShortestPaths {
-
+protected:
+    unordered_map<int, TValue> vis;
+    unordered_map<int, TValue> cost;
+    unordered_map<int, TValue> parent;
+    int source;
 public:
     ShortestPaths() {
     }
@@ -23,15 +27,26 @@ public:
 
 public:
     virtual bool HasPathTo(int destination) const {
-
+        return vis.find(destination) != vis.end();
     }
 
     virtual std::optional<TValue> TryGetDistanceTo(int destination) const {
-
+        if(vis.find(destination) != vis.end()) {
+            return optional<TValue>(cost.find(destination)->second);
+        } else return nullopt;
     }
 
     virtual std::optional<std::vector<int>> TryGetShortestPathTo(int destination) const {
-
+        vector<int> ans;
+        int cur = destination;
+        if(vis.find(destination) != vis.end()) {
+            while (cur != source) {
+                ans.insert(ans.begin(),cur);
+                cur = parent.find(cur)->first;
+            }
+            ans.insert(ans.begin(),cur);
+            return ans;
+        } else return nullopt;
     }
 };
 
